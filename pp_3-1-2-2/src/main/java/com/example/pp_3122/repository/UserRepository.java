be7e55768dbfa7ca20b1269c6ce8b6b9a6dc15b2
YalokN <118694186+YalokN@users.noreply.github.com>
2023-05-15 16:58:59 +0500

    Delete pp_3-1-2-2 directory

--- pp_3-1-2-2/src/main/java/com/example/pp_3122/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pp_3122.repository;
-
-import com.example.pp_3122.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- pp_3-1-2-2/src/main/java/com/example/pp_3122/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.pp_3122.service;
-
-import com.example.pp_3122.model.User;
-
-public interface UserService {
-    public void addUser(User user);
-}
